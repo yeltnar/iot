22e161cce8aa48bb58d75e6c23221e5c4c1c0a1e
Drew Brantley <brantley@us.ibm.com>
2018-03-19 15:49:11 -0500

    now can connect to bm socket server and receive commands

--- app.ts
+++ app.ts
@@ -5,14 +5,16 @@ import {Things, Thing} from "./class/thing";
 import {aliasInit} from "./alias";
 import {googleDriveInit} from "./connectors/googleDrive";
 import {hueInit} from "./connectors/hue";
+import {connectToSocketInit} from "./connectors/connectToSocket";
 
 let things = new Things("Drew's IOT");
 
 iftttInit(config.ifttt, things);
 endpointsInit(config.express, things);
 hueInit(config.hue, things);
-aliasInit(things); // this needs to be last
 googleDriveInit(config.googleDrive, things);
+connectToSocketInit(config.socket, things);
+aliasInit(things); // this needs to be last
 
 //things.getThing("notification").callCallback("notify", "title", "message "+(Date.now().toString()), "http://google.com");
 // things.getThing("desk_socket").callCallback("off");
--- connectors/connectToSocket.ts
+++ connectors/connectToSocket.ts
@@ -1,11 +1,43 @@
+import {Things, Thing} from "../class/thing";
 const WebSocket = require('ws');
+
+function connectToSocketInit(socketConfig, things:Things){
  
-const ws = new WebSocket('ws://www.host.com/path');
- 
-ws.on('open', function open() {
-  ws.send('something');
-});
- 
-ws.on('message', function incoming(data) {
-  console.log(data);
-});
+	const ws = new WebSocket(socketConfig.url);
+	 
+	ws.on('open', function open() {
+
+		let obj = {
+	        'cmd': 'connected',
+	        'group':'home_pi',
+	        'userType':"node"
+	    }
+	  	ws.send( JSON.stringify(obj) );
+	  	console.log("socket open")
+	});
+	 
+	ws.on('message', function incoming(data) {
+		if( data.indexOf("Welcome, new ")<0 ){
+			try{
+				data = JSON.parse(data);
+				let {thing, callback, params} = data;
+
+				if( thing!==undefined && callback!==undefined ){
+					if( params!==undefined ){
+						things.getThing(thing).callCallback(callback, ...params);
+					}else{
+						things.getThing(thing).callCallback(callback);	
+					}
+				}
+			}catch(e){
+				console.error(e);
+				console.log(data);
+			}
+		}else{
+			console.log(data);
+		}
+	});
+
+}
+
+export {connectToSocketInit}
