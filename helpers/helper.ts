74f37b300f69032b0e26d0c7dfe14e3b1fde7aab
Drew Brantley <brantley@us.ibm.com>
2018-03-23 12:30:07 -0500

    added execPromise

--- helpers/helper.ts
+++ helpers/helper.ts
@@ -1,9 +1,24 @@
+const {exec} = require("child_process")
+
 let helpers = {
 
 	timeoutPromise:(timeout)=>{
 		return new Promise((resolve, reject)=>{
 			setTimeout( resolve, timeout );
 		});
+	},
+	execPromise:(command)=>{
+		return new Promise((resolve, reject)=>{
+
+			exec(command, (err, stdout, stderr)=>{
+				if(err){
+					return reject(err);
+				}else if(stderr){
+					return reject(stderr);
+				}
+				return resolve(stdout);
+			});
+		})
 	}
 
 };
