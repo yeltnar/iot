054fd95c67b5e5a4d44ca339db18cd7c446be404
Drew Brantley <brantley@us.ibm.com>
2018-03-23 13:21:40 -0500

    keep alive

--- helpers/keep_bm_alive.sh
+++ helpers/keep_bm_alive.sh
@@ -1,9 +1,10 @@
 DIR="~/bm_iot_keep_alive"
 
-rm -rf DIR;
-mkdir DIR
-git clone "https://git.ng.bluemix.net/andcbrant/drew-iot-new.git" DIR
-date > DIR/date.txt
+rm -rf $DIR;
+mkdir $DIR
+cd $DIR
+git clone "https://git.ng.bluemix.net/andcbrant/drew-iot-new.git" $DIR
+date > $DIR/date.txt
 git add .
 git commit -m "keep alive"
 git push
